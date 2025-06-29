c53c9db07e3b2dece4e9d148753b14e07914466f
bneuhausz <bneuhausz@gmail.com>
2025-06-29 17:12:41 +0200

    added @angular/pwa and configured request caching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <h1>Welcome to pwa-demo - V7!</h1>
 
     <router-outlet />
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'pwa-demo';
+  private readonly swUpdate = inject(SwUpdate);
+
+  constructor() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates
+        .pipe(filter(event => event.type === 'VERSION_READY'))
+        .subscribe((event: VersionReadyEvent) => {
+          if (confirm(`A new version (${event.latestVersion.hash}) is available. Load New Version?`)) {
+            window.location.reload();
+          }
+        });
+    }
+  }
 }
